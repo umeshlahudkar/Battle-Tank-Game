d9f5d802f4d3bb9d846601ecd5c53889f5c5da49
Umesh Lahudkar <umeshlahudkar@gmail.com>
2022-07-06 09:15:40 +0530

    Movement

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -6,14 +6,31 @@ public class TankController
 {
     private TankModel tankModel;
     private TankView tankView;
+    private Rigidbody rb;
 
     public TankController(TankModel _tankModel, TankView _tankView)
     {
         tankModel = _tankModel;
         tankView = GameObject.Instantiate<TankView>(_tankView);
+        rb = tankView.GetComponent<Rigidbody>();
 
         tankModel.setTankController(this);
         tankView.setTankController(this);
+    }
+
+    public void Move(float movement, float movementSpeed)
+    {
+        rb.velocity = tankView.transform.forward * movement * movementSpeed ;
+    }
+
+    public void Rotate(float rotation, float rotationSpeed)
+    {
+        tankView.transform.Rotate(0, rotation * rotationSpeed * Time.deltaTime , 0);
+    }
+
+    public TankModel GetTankModel()
+    {
+        return tankModel;
     }
 
 }
